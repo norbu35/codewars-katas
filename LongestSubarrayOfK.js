be21fe1c1bfbdfa19a42c55ca10af1ba60362d79
norbu <93921032+norbu35@users.noreply.github.com>
2023-04-10 21:24:30 +0800

    added problem description and source

--- LongestSubarrayOfK.js
+++ LongestSubarrayOfK.js
@@ -1,3 +1,11 @@
+// https://www.codewars.com/kata/58feb7ac627d2fdadf000111
+
+// Given an array of positive integers a and an integer k, find the first and last index of the longest subarray of a that consists only of k.
+
+// If the array contains multiple subarrays of the same length, return indices of the last one.
+
+// If k doesn't exist in a, return (-1, -1).
+
 function findSubArrayWithSameElement(a, k) {
   let maxLength = 0;
   let currentLength = 0;
